154efef816cfbfefd9f013d3b7349bd76486c1ed
pvdec <diogo@decifrar.net>
2023-04-03 11:08:14 +0200

    basic layout

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+
+export default function Layout() {
+    return (
+        <div id="layout">
+            <header>
+            </header>
+            <main>
+            </main>
+            <footer>
+            </footer>
+        </div>
+    )
+}
